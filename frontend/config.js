c90ce251f7b06e200c9c3cd58143eca9ef71afd9
FFarquar <ffarquar21@gmail.com>
2026-06-18 11:18:38 +1000

    Have a staging and production front end website now. First try

--- frontend/config.js
+++ frontend/config.js
@@ -1,10 +1,8 @@
 // config.js (Local development default). Here to allow CICD to update Base URLS in HTML pages
 // just some text to force re-commit
 window.APP_CONFIG = { API_BASE_URL: "http://localhost:3000",
-
-    
+  ENVIRONMENT: 'LOCAL',
   // 🔥 MASTER SWITCH
   USE_MOCK: false
-  
  };
 
